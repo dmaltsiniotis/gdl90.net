9405832c437083f2976db8079ef2e36ad65dd533
Demetri Maltsiniotis <git@quantumhive.com>
2026-08-18 20:39:33 -0500

    Fix usage, add benchmark flag, remove last vestigaes of Console.WriteLine and use the Logger instead.

--- gdl90_console/Program.cs
+++ gdl90_console/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
-using System.Net.Sockets;
 using System.Text;
 
 namespace GDL90 {
@@ -14,35 +12,42 @@ namespace GDL90 {
         }
 
         public ProgramMode Mode = ProgramMode.ListenUDP;
-        public string InFile = ""; // Input from a recorded file.
-        public string OutFile = ""; // Output to a recorded file.
-        public bool Force; // Make sure options that don't make sense together are intended.
-        public bool Stats; // Enable statistics output.
+        public string InFile = String.Empty; // Input from a recorded file.
+        public string OutFile = String.Empty; // Output to a recorded file.
+        public bool Force = false; // Make sure options that don't make sense together are intended.
+        public bool Stats = false; // Enable statistics output.
+        public bool Benchmark = false; // Enable benchmark mode.
         public int UdpListenPort = 4000; // Default 4000 for Stratux GDL90
         public Logging.Verbosity LogLevel = Logging.Verbosity.Info;
 
-        public ProgramOptions() {
-
+        public ProgramOptions()
+        {
+            
         }
     }
 
     public class Processor {
+        private readonly Logging.Logger logger;
         private readonly ProgramOptions options;
-        public int receivedBytesBeforeFlush = 0;
-        public FileStream? binaryStratuxDataFile = null;
+        private readonly int flushThreshold = 1024;
+        private int receivedBytesBeforeFlush = 0;
+        private FileStream? binaryStratuxDataFile = null;
         private readonly AsyncCallback GDL90MessageReceivedCallbackDelegate;
         private readonly AsyncCallback StreamCorruptionCallbackDelegate;
         private int MessageCount = 0;
         private int MessageCountBadCRC = 0;
         private int StreamCorruptionCount = 0;
-        private Logging.Logger logger;
+        
         private readonly UDPListener UdpListener = new UDPListener();
         private readonly MessageStreamParser MessageStreamParser;
 
-        public Processor(ProgramOptions options) {
+        public Processor(ProgramOptions options, Logging.Logger logger) {
             this.options = options;
-            this.logger = new Logging.Logger(options.LogLevel);
-            if (!string.IsNullOrEmpty(options.OutFile)) {
+            this.logger = logger;
+
+            logger.Info("GDL90 Decoder v0.2.0");
+
+            if (!String.IsNullOrEmpty(options.OutFile)) {
                  binaryStratuxDataFile = new FileStream(options.OutFile, FileMode.OpenOrCreate | FileMode.Append, FileAccess.Write);
             }
             GDL90MessageReceivedCallbackDelegate = new AsyncCallback(ProcessMessage);
@@ -56,6 +61,7 @@ namespace GDL90 {
             logger.Warn(String.Format("{0} {1:0000000} Stream corruption detected. Discarding and re-framing.", DateTimeOffset.Now.ToUnixTimeMilliseconds(), MessageCount));
             StreamCorruptionCount++;
         }
+
         private void ProcessMessage(IAsyncResult ar) {
             if (ar.AsyncState == null)
             {
@@ -71,11 +77,11 @@ namespace GDL90 {
                 logger.Debug(String.Format("{0} {1:0000000} {2}", DateTimeOffset.Now.ToUnixTimeMilliseconds(), MessageCount, newGDL90Message.ToShortString()));
 
                 // If we're logging to file, dump the message bytes back out.
-                if (!string.IsNullOrEmpty(options.OutFile) && binaryStratuxDataFile != null && binaryStratuxDataFile.CanWrite) {
+                if (!String.IsNullOrEmpty(options.OutFile) && binaryStratuxDataFile != null && binaryStratuxDataFile.CanWrite) {
                     receivedBytesBeforeFlush += newGDL90Message.MessageFrame.Length;
                     binaryStratuxDataFile.Write(newGDL90Message.MessageFrame);
-                    if (receivedBytesBeforeFlush >= 1024) {
-                        Console.WriteLine("Flushing save file contents...");
+                    if (receivedBytesBeforeFlush >= flushThreshold) {
+                        logger.Info("Flushing save file contents...");
                         binaryStratuxDataFile.Flush(true);
                         receivedBytesBeforeFlush = 0;
                     }
@@ -91,16 +97,11 @@ namespace GDL90 {
         private void ReadFromFileStream(Stream dataStream)
         {
             MessageStreamParser.StartAsync(dataStream);
-            
             while(dataStream.Position < dataStream.Length)
             {
-                System.Threading.Thread.Sleep(10); // Sleep for a second to avoid busy waiting.
-                // Wait for the stream to finish reading.
+                System.Threading.Thread.Sleep(100);
             }
-            // TODO: Put this behind a an optional input flag.
-            // Console.WriteLine("Press any key to end...");
-            // Console.ReadLine();
-            Console.WriteLine("Done reading from file, exiting.");
+            logger.Info("Done reading from file.");
         }
 
         public void Start() {
@@ -116,21 +117,28 @@ namespace GDL90 {
                     ReadFromFileStream(File.OpenRead(options.InFile));
                     break;
             }
+            if (options.Benchmark == false)
+            {
+                logger.Info("Press any key to exit...");
+                Console.ReadLine();
+            }
             stopwatch.Stop();
-            logger.Info("Done.");
+            logger.Info("Done, exiting.");
 
             if(options.Stats)
             {
+                logger.Info("Stats:");
                 logger.Info(String.Format("Processed {0:N0} messages in {1:N0} milliseconds, a rate of {2:N2} messages per second.", MessageCount, stopwatch.ElapsedMilliseconds, ((double)MessageCount / (double)stopwatch.ElapsedMilliseconds * 1000.0).ToString("00.00")));
                 logger.Info(String.Format("Valid CRC: {0:N0}. Failed CRC: {1:N0} ({2:P})", MessageCount - MessageCountBadCRC, MessageCountBadCRC, (double)MessageCountBadCRC / (double)MessageCount));
                 logger.Info(String.Format("Stream corruptions detected: {0:N0}.", StreamCorruptionCount));
             }
+            System.Environment.Exit(0);
         }
     }
+
     public class Program {
-        private static ProgramOptions ParseArgs(string[] args)
+        private static ProgramOptions ParseArgs(string[] args, Logging.Logger logger)
         {
-            Console.WriteLine("GDL90 Decoder v0.1.0");
             ProgramOptions options = new();
             for (int i = 0; i < args.Length; i++)
             {
@@ -155,9 +163,12 @@ namespace GDL90 {
                     case "--stats":
                         options.Stats = true;
                         break;
+                    case "--benchmark":
+                        options.Benchmark = true;
+                        break;
                     case "--help":
                     default:
-                        PrintUsage();
+                        logger.Info(GetUsage());
                         Environment.Exit(0);
                         break;
                 }
@@ -165,17 +176,15 @@ namespace GDL90 {
 
             if (!string.IsNullOrEmpty(options.OutFile) && !string.IsNullOrEmpty(options.InFile) && options.Force == false)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("WARN: --outfile and --infile used together don't make sense. Use --force to proceed anyway.");
-                Console.ResetColor();
-                PrintUsage();
+                logger.Warn("--outfile and --infile used together don't make sense. Use --force to proceed anyway.");
+                logger.Info(GetUsage());
                 Environment.Exit(1);
             }
 
             return options;
         }
         
-        private static void PrintUsage()
+        private static string GetUsage()
         {
             StringBuilder usageString = new StringBuilder();
             usageString.AppendLine("GDL90 Decoder Usage:");
@@ -183,9 +192,10 @@ namespace GDL90 {
             usageString.AppendLine("  --infile \"data.bin\" (Reads from a binary file rather than a UDP port.)");
             usageString.AppendLine("  --outfile \"data.bin\" (Saves binary data received to a file before processing.)");
             usageString.AppendLine("  --port 4000 (Use a different UDP port to listen for GLD90 messages. Default 4000.)");
-            usageString.AppendLine("  --loglevel 0-4 (Silent=0, Error=1, Warn=2, Info=3, Debug=4. Default is 1)");
+            usageString.AppendLine("  --loglevel 0-4 (Silent=0, Error=1, Warn=2, Info=3, Debug=4. Default is 3 - Info.)");
             usageString.AppendLine("  --force (Continue anyway even if weird arguments are supplied, such as both --infile and --outfile)");
             usageString.AppendLine("  --stats (Enable statistics output after processing.)");
+            usageString.AppendLine("  --benchmark (Enable benchmark mode, does not wait for user input to exit after reading all data.)");
             usageString.AppendLine("Example 1:");
             usageString.AppendLine("gdl90.exe (Start with default options, listening on UDP port 4000.)");
             usageString.AppendLine("");
@@ -195,11 +205,13 @@ namespace GDL90 {
             usageString.AppendLine("Example 2:");
             usageString.AppendLine("gdl90.exe --infile \"gdl90data.bin\" (Start and read from a raw data to file called gdl90data.bin.)");
             usageString.AppendLine("");
-            Console.WriteLine(usageString);
+            return usageString.ToString();
         }
-        
+
         public static int Main(params string[] args) {
-            new Processor(ParseArgs(args)).Start();
+            Logging.Logger logger = new Logging.Logger(Logging.Verbosity.Info); // Create a logger with default log level (Info) until we parse the args and know what the user wants.
+            ProgramOptions Options = ParseArgs(args, logger);
+            new Processor(Options, logger).Start();
             return 0;
         }
     }
